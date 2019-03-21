0092e4692476efb6266a4d09a680b58b1d07cc11
Leonid Zhdanok <leonid.zhdanok@gmail.com>
2019-03-21 21:04:41 +0300

    feat: add approval page

---
+++ our-dream-project/src/app/approval-table/approval-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-table',
+  templateUrl: './approval-table.component.html',
+  styleUrls: ['./approval-table.component.scss']
+})
+export class ApprovalTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
